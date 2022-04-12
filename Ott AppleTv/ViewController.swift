445dc94fb5afe5846170d67d6e7ae9e569c6bcdb
SaiKiran Panuganti <saikiranpanuganti92@gmail.com>
2022-04-13 00:20:41 +0530

    Initial Commit

---
+++ Ott AppleTv/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ott AppleTv
+//
+//  Created by Saikiran Panuganti on 13/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
